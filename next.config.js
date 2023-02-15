0d653a5e01e79645a2a22ed14dd96479edbdeba5
mypp-hub <2088825249@qq.com>
2023-02-15 03:19:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
